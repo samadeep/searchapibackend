58cfea49732c2e1de1930d9b199f69ca4f060066
samadeep <samadeepsengupta@gmail.com>
2025-02-28 12:24:59 +0530

    Intial Commit

---
+++ src/main/java/com/search/searchapi/SearchapiApplication.java
@@ -0,0 +1,13 @@
+package com.search.searchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/search/searchapi/SearchapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.searchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
